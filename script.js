ae71420cf256536d668a291292e48c871dfb6c39
Rafi <edianantarafi@gmail.com>
2022-05-18 09:15:23 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+const Name = prompt("What's Your Name?")
+
+
+const user = {
+    Name : Name
+}
+
+alert("Nice to meet you " + user.Name + " " +"!");
+alert("Welcome to my first website")
+alert("I hope you enjoy")
+
